2730292b9ad60c24b8c2ca119e4d96e7785e65a2
Brendan Cariota <brendan@cariota.com>
2021-02-06 16:06:36 -0600

    Project Setup

---
+++ src/components/StockRow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StockRow = ({ ticker }) => {
+    return (
+        <tr>
+            <td>{ticker}</td>
+            <td>sfsf</td>
+            <td>adsf</td>
+            <td>saf</td>
+        </tr>
+    )
+}
+
+export default StockRow
