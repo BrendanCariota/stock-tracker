0d87f63b50d933c41e6dc898fc76e8042962c75f
Brendan Cariota <brendan@cariota.com>
2021-02-07 00:43:57 -0600

    Add first basic endpoint

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {iex} from '../config/iex'
 
 const StockRow = ({ ticker }) => {
+
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        // query the API
+        const url = `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`
+
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setData( data[data.length - 1] )
+        })
+
+        
+    }, [ticker])
+
     return (
         <tr>
             <td>{ticker}</td>
-            <td>sfsf</td>
-            <td>adsf</td>
-            <td>saf</td>
+            <td>{data.close}</td>
+            <td>{data.date}</td>
+            <td>{data.label}</td>
         </tr>
     )
 }
---
+++ src/config/iex.js
@@ -0,0 +1,4 @@
+export const iex = {
+    api_token: 'pk_bfc419472ec24802ba540f154468f44a ',
+    base_url: 'https://cloud.iexapis.com/stable'
+}
